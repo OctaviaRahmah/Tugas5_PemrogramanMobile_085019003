b280e9fa95020d187c197254d1012c75397dd8d1
OctaviaRahmah <81372443+OctaviaRahmah@users.noreply.github.com>
2022-01-08 12:28:39 +0700

    Delete Tugas5 directory

--- Tugas5/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:src/pages/index.dart';
-import 'package:src/themes/color.dart';
-
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primaryColor: primary),
-      home: IndexPage(),
-    ));
